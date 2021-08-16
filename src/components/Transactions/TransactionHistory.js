cb77ed9f31296a89f931ebf43473beefb303cd6a
Sergey-Markov <1nativefl@gmail.com>
2021-08-16 17:50:08 +0300

    add table style and classes

--- src/components/Transactions/TransactionHistory.js
+++ src/components/Transactions/TransactionHistory.js
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types';
+import s from './transactionHistory.module.css';
 
 export default function TransactionHistory({items}){
     return(
-        <table className="transaction-history">
+        <table className={s.transactionHistory}>
   <thead>
     <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+      <th className={s.th}>Type</th>
+      <th className={s.th}>Amount</th>
+      <th className={s.th}>Currency</th>
     </tr>
   </thead>
 
   {items.map(item =>(
       <tbody key={item.id}>
       <tr>
-        <td>{item.type}</td>
-        <td>{item.amount}</td>
-        <td>{item.currency}</td>
+        <td className={s.td}>{item.type}</td>
+        <td className={s.td}>{item.amount}</td>
+        <td className={s.td}>{item.currency}</td>
       </tr>
     </tbody>
   ))}
